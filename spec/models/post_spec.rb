04887cff9b1f474b4a89b527da8af42ed304fae8
CamBrown101 <cameron-brown@live.com>
2021-09-16 12:53:39 -0700

    fix typo

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it "is valid with a title and body"
-  is "is invalid without a title"
-  is "is invalid without a description"
+  it "is invalid without a title"
+  it "is invalid without a description"
 end
