d13cc287ec1ded5f61f628d00e594d5b28f80e1f
CamBrown101 <cameron-brown@live.com>
2021-09-16 13:19:43 -0700

    Write Post model tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it "is valid with a title and body"
-  it "is invalid without a title"
-  it "is invalid without a description"
+  it "is valid with a title and body" do
+    post = Post.new(
+      title: "Hello",
+      body: "World"
+    )
+    expect(post).to be_valid
+  end
+
+  it "is invalid without a title" do
+    post = Post.new(title: nil)
+    post.valid?
+    expect(post.errors[:title]).to include("can't be blank")
+  end
+  
+  it "is invalid without a body" do
+    post = Post.new(body: nil)
+    post.valid?
+    expect(post.errors[:body]).to include("can't be blank")
+  end
 end
