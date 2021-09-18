5e3550d8fbbb3c174804d510b63433261d723c70
CamBrown101 <cameron-brown@live.com>
2021-09-18 11:58:21 -0700

    Use factory and add test to validate factory

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,23 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
+describe Post, type: :model do
+  it "has a valid factory" do
+    expect(FactoryBot.build(:post)).to be_valid
+  end
+
   it "is valid with a title and body" do
-    post = Post.new(
-      title: "Hello",
-      body: "World"
-    )
+    post = FactoryBot.build(:post)
     expect(post).to be_valid
   end
 
   it "is invalid without a title" do
-    post = Post.new(title: nil)
+    post = FactoryBot.build(:post, title: nil)
     post.valid?
     expect(post.errors[:title]).to include("can't be blank")
   end
   
   it "is invalid without a body" do
-    post = Post.new(body: nil)
-    post.valid?
+    post = FactoryBot.build(:post, body: nil)
+        post.valid?
     expect(post.errors[:body]).to include("can't be blank")
   end
 end
