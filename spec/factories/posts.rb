b21b1074d5689c020213b4123910edfd712e0bff
CamBrown101 <cameron-brown@live.com>
2021-09-18 12:27:56 -0700

    Add invalid post factories

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :post do
     title "This is a title"
     body "This is the body"
+
+    factory :post_with_invalid_title do
+      title ""
+    end
+
+    factory :post_with_invalid_body do
+      body ""
+    end
   end
 end
