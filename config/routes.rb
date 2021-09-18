73caa4c48ddc47fda946ac8bb91439c8b5b49495
CamBrown101 <cameron-brown@live.com>
2021-09-18 13:50:08 -0700

    Add main controller test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "hello_world", to: "hello#index"
 
-  root to: "main#index"
+  root "main#index"
   resources :posts, only: [:new, :create, :show]
 end
