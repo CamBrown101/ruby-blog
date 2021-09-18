e67de79a3c3c3506c9e77ba6289c405e7687b311
CamBrown101 <cameron-brown@live.com>
2021-09-18 14:51:21 -0700

    add post controller tests

---
+++ spec/controllers/post_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+    describe "#new" do
+        it "responds successfully" do
+            post_params = FactoryBot.attributes_for(:post)
+            post :new, params: { post: post_params }
+            expect(response).to be_successful
+        end
+
+        it "returns a 200" do
+            post_params = FactoryBot.attributes_for(:post)
+            post :new, params: { post: post_params }
+            expect(response).to have_http_status "200"
+        end
+    end
+
+    describe "#create" do
+        it "returns a 302" do
+            post_params = FactoryBot.attributes_for(:post)
+            post :create, params: { post: post_params }
+            expect(response).to have_http_status "302"
+        end
+    end
+
+    describe "#show" do
+        it "returns a 200" do
+            @post = FactoryBot.create(:post)
+            get :show, params: { id: @post.id }
+            expect(response).to have_http_status "200"
+        end
+    end
+end
