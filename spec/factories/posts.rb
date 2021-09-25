411789232dd0f72e5462fd057f951110b8fed6fa
CamBrown101 <cameron-brown@live.com>
2021-09-25 16:05:56 -0700

    Remove invalid trait

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,13 +2,5 @@ FactoryBot.define do
   factory :post do
     title { Faker::Book.title }
     body { Faker::Quote}
-
-    trait :invalid_title do
-      title ""
-    end
-
-    trait :invalid_body do
-      body ""
-    end
   end
 end
