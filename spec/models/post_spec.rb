710e51807cd0646f48474f281615605ac0a0f763
CamBrown101 <cameron-brown@live.com>
2021-09-25 16:16:07 -0700

    Fix tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -25,6 +25,6 @@ describe Post, type: :model do
   it "is invalid without a body and title" do
     post = FactoryBot.build(:post, body: nil, title: nil)
         post.valid?
-    expect(post.errors[:body]).to include("can't be bjhgjhgjlank")
+    expect(post.errors[:body]).to include("can't be blank")
   end
 end
