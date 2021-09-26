7c39e5792c9683317e7190b8a753b32414a6a0c7
CamBrown101 <cameron-brown@live.com>
2021-09-26 15:56:24 -0700

    Fix spacing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,29 @@
 class PostsController < ApplicationController
-    def index
-      @posts = Post.all
-      render :index
-    end
+  def index
+    @posts = Post.all
+    render :index
+  end
 
-    def new
-        @post = Post.new
-    end
+  def new
+    @post = Post.new
+  end
 
-    def create
-        @post = Post.new(post_params)
-        if @post.save
-            redirect_to post_path(@post)
-        else
-            render :new
-        end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
     end
+  end
 
-    def show
-        @post = Post.find(params[:id])
-    end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-    private
+  private
 
-    def post_params
-        params.require(:post).permit(:title, :body)
-    end
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
