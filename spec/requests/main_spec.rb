2d89228282f7b411d5c601cc46235ca16a227a6e
CamBrown101 <cameron-brown@live.com>
2021-09-26 16:09:46 -0700

    aggregate_failures

--- spec/requests/main_spec.rb
+++ spec/requests/main_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe "Main page", type: :request do
   it "responds successfully" do
     get root_path
-    expect(response).to be_successful
-    expect(response).to have_http_status "200"
+    aggregate_failures do
+      expect(response).to be_successful
+      expect(response).to have_http_status "200"
+    end
   end
 end
