ada0fbde597dc0aa5834510af08d3868ba8a51d9
CamBrown101 <cameron-brown@live.com>
2021-10-08 15:09:27 -0500

    Add model test to check title uniqueness

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -15,16 +15,23 @@ describe Post, type: :model do
     post.valid?
     expect(post.errors[:title]).to include("can't be blank")
   end
+
+  it "is invalid without a unique title" do
+    post_one = FactoryBot.create(:post, title: "hello")
+    post_two = FactoryBot.build(:post, title: "hello")
+    post_two.valid?
+    expect(post_two.errors[:title]).to include("has already been taken")
+  end
   
   it "is invalid without a body" do
     post = FactoryBot.build(:post, body: nil)
-        post.valid?
+    post.valid?
     expect(post.errors[:body]).to include("can't be blank")
   end
 
   it "is invalid without a body and title" do
     post = FactoryBot.build(:post, body: nil, title: nil)
-        post.valid?
+    post.valid?
     expect(post.errors[:body]).to include("can't be blank")
   end
 end
