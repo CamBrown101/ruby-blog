c66129563ec2a247dc112f38394fecf4059a8f84
CamBrown101 <cameron-brown@live.com>
2021-10-08 14:17:16 -0500

    Add uniquness to title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
+  validates :title, uniqueness: true, presence: true
   validates :body, presence: true
   after_validation :set_slug, only: [:create]
 
