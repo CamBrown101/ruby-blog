ca67a8c9a85688a0cf2b967a93714867ac5eb77e
CamBrown101 <cameron-brown@live.com>
2021-10-31 13:41:39 -0700

    Create User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,3 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
   validates :username, presence: true, uniqueness: true
 end
-
