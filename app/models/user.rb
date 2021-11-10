38786de35ab2fb0b76dff575e24f91b17aaa7e9f
CamBrown101 <cameron-brown@live.com>
2021-11-10 08:32:34 -0800

    Add User methods to generate token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ApplicationRecord
   # validates email
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
   validates :username, presence: true, uniqueness: true
+  def generate_password_reset_token
+    self.reset_password_token = SecureRandom.hex
+    self.reset_password_sent_at = Time.now.utc
+    self.save
+  end
+
+  def password_token_valid?
+    (self.reset_password_sent_at + 4.hours) > Time.now.utc
+  end
+
+  def reset_password(password)
+    self.reset_password_token = nil
+    self.password = password
+    self.save
+  end
 end
